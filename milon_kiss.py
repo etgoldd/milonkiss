b513d97ec951942564695ac2e9d8f5bf44b552bf
etgoldd <etgold05@gmail.com>
2025-11-04 16:57:32 +0200

    Fixed some issues in PR

--- milon_kiss.py
+++ milon_kiss.py
@@ -2,56 +2,51 @@ from typing import Callable
 import sys
 
 
-DICTIONARY: dict[str, str] = {}
-
-
-def add_word() -> None:
+def add_word(dictionary: dict[str, str]) -> None:
     word = input("Word to add: ")
     meaning = input(f'Meaning of "{word}": ')
-    DICTIONARY.update({word: meaning})
+    dictionary.update({word: meaning})
     print(f"{word} has been added to the dictionary!")
 
 
-def get_word() -> None:
+def get_word(dictionary: dict[str, str]) -> None:
     word = input("Word to get: ")
-    if word in DICTIONARY.keys():
-        print(f'"{word}" means "{DICTIONARY[word]}"')
+    if word in dictionary.keys():
+        print(f'"{word}" means "{dictionary[word]}"')
     else:
         print(f'"{word}" isn\'t in this dictionary :(')
 
 
-def remove_word() -> None:
+def remove_word(dictionary: dict[str, str]) -> None:
     word = input("Word to remove: ")
-    if word in DICTIONARY.keys():
-        DICTIONARY.pop(word)
+    if word in dictionary.keys():
+        dictionary.pop(word)
         print(f'"{word}" has been successfully removed from the dictionary')
     else:
         print(f'"{word}" isn\'t in this dictionary :(')
 
 
-def on_exit() -> None:
-    print("Goodbye!")
-    sys.exit(0)
-
-
-MENU_OPTIONS: dict[int, tuple[str, Callable]] = {
-    1: ("Get word", get_word),
-    2: ("Add word", add_word),
-    3: ("Delete word", remove_word),
-    4: ("Exit", on_exit),
-}
-
-
-# val[0] refers to the name, val[1] the function that matches
-MENU_TEXT = "\n".join([f"{i}: {val[0]}" for i, val in MENU_OPTIONS.items()]) + "\nEnter choice: "
-
-
 def main() -> None:
-    selections = {key: val[1] for key, val in MENU_OPTIONS.items()}
-    while (choice := int(input(MENU_TEXT))):
-        if choice not in MENU_OPTIONS.keys():
+    dictionary: dict[str, str] = {}
+
+    menu_options: dict[int, tuple[str, Callable]] = {
+        1: ("Get word", get_word),
+        2: ("Add word", add_word),
+        3: ("Delete word", remove_word),
+    }
+    selections: dict[int, Callable] = {key: val[1] for key, val in menu_options.items()}
+    # val[0] refers to the name, val[1] the function that matches
+    menu_text = "0: Exit\n"
+    menu_text += "\n".join([f"{i}: {val[0]}" for i, val in menu_options.items()])
+    menu_text += "\nEnter choice: "
+
+    while (choice := int(input(menu_text))):
+        if choice == 0:
+            print("Goodbye!")
+            break
+        if choice not in menu_options.keys():
             continue
-        selections[choice]()
+        selections[choice](dictionary)
 
 
 if __name__ == '__main__':
